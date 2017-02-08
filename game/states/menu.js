7f0ffb68a995b11605d296a9ce920b07672deaac
William Oliveira <sudowilliam@gmail.com>
2017-02-08 07:20:02 -0200

    Remove unused code

--- game/states/menu.js
+++ game/states/menu.js
@@ -1,14 +1,5 @@
 class Menu extends Phaser.State {
     create () {
-        var nameLabel = this.game.add.text(80, 80, 'Pong', { font: '30px Courier', fill: '#FFF' });
-
-        var startLabel = this.game.add.text(
-            80,
-            this.game.world.height - 80,
-            'press space to start',
-            { font: '30px Courier', fill: '#FFF' }
-        );
-
         this.game.stage.smoothed = false;
 
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
